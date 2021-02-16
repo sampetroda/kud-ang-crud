41eeadfaaf52394a1c094661d844839cbb79d6c7
rajugaddam1224 <gaddam_raju@outlook.com>
2021-02-16 10:39:44 +0530

    Reading employee data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListEmployeesComponent } from './employees/list-employees/list-employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmployeesComponent
   ],
   imports: [
     BrowserModule,
